58bfccdddc642c9dc0ccf0275823fec4dd9f7679
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2024-01-25 12:24:26 -0500

    feat: pingaclweak

--- pingaclweak_envoy/src/lib.rs
+++ pingaclweak_envoy/src/lib.rs
@@ -4,10 +4,8 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 
 use prost::Message;
 pub mod ping {
-    include!(concat!(env!("OUT_DIR"), "/ping_pb.rs"));
+    include!(concat!(env!("OUT_DIR"), "/ping.rs"));
 }
-
-static GLOBAL_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
 #[no_mangle]
 pub fn _start() {
@@ -26,7 +24,6 @@ impl Context for AccessControl {}
 
 impl HttpContext for AccessControl {
     fn on_http_request_headers(&mut self, _num_of_headers: usize, end_of_stream: bool) -> Action {
-        log::warn!("executing on_http_request_headers");
         if !end_of_stream {
             return Action::Continue;
         }
@@ -36,31 +33,18 @@ impl HttpContext for AccessControl {
     }
 
     fn on_http_request_body(&mut self, body_size: usize, end_of_stream: bool) -> Action {
-        log::warn!("executing on_http_request_body");
         if !end_of_stream {
             return Action::Pause;
         }
 
-        // Replace the message body if it contains the text "secret".
-        // Since we returned "Pause" previuously, this will return the whole body.
         if let Some(body) = self.get_http_request_body(0, body_size) {
-            // log::warn!("body: {:?}", body);
-            // Parse grpc payload, skip the first 5 bytes
             match ping::PingEchoRequest::decode(&body[5..]) {
                 Ok(req) => {
-                    // log::info!("req: {:?}", req);
-                    // log::warn!("body.len(): {}", req.body.len());
-                    // log::warn!("body : {}", req.body);
                     if req.body == "test" {
-                        // self.abort_count += 1; // Increment the counter
-                        // log::warn!("Aborting a request!!!! Abort Count: {}", self.abort_count);
-                        let counter_val = GLOBAL_COUNTER.fetch_add(1, Ordering::SeqCst) + 1;
-                        log::warn!("Global counter value: {}", counter_val);
                         self.send_http_response(
                             403,
                             vec![
                                 ("grpc-status", "7"),
-                                // ("grpc-message", "Access forbidden.\n"),
                             ],
                             None,
                         );
@@ -75,7 +59,6 @@ impl HttpContext for AccessControl {
     }
 
     fn on_http_response_headers(&mut self, _num_headers: usize, end_of_stream: bool) -> Action {
-        log::warn!("executing on_http_response_headers");
         if !end_of_stream {
             return Action::Continue;
         }
@@ -84,7 +67,6 @@ impl HttpContext for AccessControl {
     }
 
     fn on_http_response_body(&mut self, _body_size: usize, end_of_stream: bool) -> Action {
-        log::warn!("executing on_http_response_body");
         if !end_of_stream {
             return Action::Pause;
         }
