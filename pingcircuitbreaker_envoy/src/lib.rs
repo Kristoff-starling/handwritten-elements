5e310e87966fd0159e05eb763b6dd2f11d34406c
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2024-01-27 12:14:32 -0500

    fix: circuitbreaker

--- pingcircuitbreaker_envoy/src/lib.rs
+++ pingcircuitbreaker_envoy/src/lib.rs
@@ -2,6 +2,7 @@ use lazy_static::lazy_static;
 use proxy_wasm::traits::{Context, HttpContext, RootContext};
 use proxy_wasm::types::{Action, LogLevel};
 use std::sync::RwLock;
+use std::collections::HashSet;
 
 pub mod ping {
     include!(concat!(env!("OUT_DIR"), "/ping.rs"));
@@ -9,6 +10,7 @@ pub mod ping {
 
 lazy_static! {
     static ref PENDING_REQ: RwLock<usize> = RwLock::new(0);
+    static ref REACHED_REQ: RwLock<HashSet<u32>> = RwLock::new(HashSet::new());
 }
 
 struct CircuitbreakerRoot;
@@ -17,6 +19,8 @@ impl Context for CircuitbreakerRoot {}
 
 impl RootContext for CircuitbreakerRoot {
     fn on_vm_start(&mut self, _: usize) -> bool {
+        let mut pending_req = PENDING_REQ.write().unwrap();
+        *pending_req = 0;
         true
     }
 }
@@ -46,27 +50,40 @@ impl Context for Circuitbreaker {
 
 impl HttpContext for Circuitbreaker {
     fn on_http_request_headers(&mut self, _num_of_headers: usize, _end_of_stream: bool) -> Action {
+        // log::warn!("[DEBUG] executing on request headers");
         Action::Continue
     }
 
     fn on_http_request_body(&mut self, _body_size: usize, _end_of_stream: bool) -> Action {
+        // log::warn!("[DEBUG] executing on request body");
         let mut pending_req = PENDING_REQ.write().unwrap();
-        *pending_req += 1;
         if *pending_req > self.max_concurrent_req {
             self.send_http_response(403, vec![("grpc-status", "1")], None);
+            return Action::Pause;
+        }
+        else {
+            *pending_req += 1;
+            let mut reached_req = REACHED_REQ.write().unwrap();
+            reached_req.insert(self.context_id);
         }
 
         Action::Continue
     }
 
     fn on_http_response_headers(&mut self, _num_headers: usize, _end_of_stream: bool) -> Action {
-        let mut pending_req = PENDING_REQ.write().unwrap();
-        *pending_req -= 1;
+        // log::warn!("[DEBUG] executing on response headers");
+        let mut reached_req = REACHED_REQ.write().unwrap();
+        if reached_req.contains(&self.context_id) {
+            let mut pending_req = PENDING_REQ.write().unwrap();
+            *pending_req -= 1;
+            reached_req.remove(&self.context_id);
+        }
 
         Action::Continue
     }
 
     fn on_http_response_body(&mut self, _body_size: usize, _end_of_stream: bool) -> Action {
+        // log::warn!("[DEBUG] executing on response body");
         Action::Continue
     }
 }
